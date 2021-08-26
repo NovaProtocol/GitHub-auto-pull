927917101d4a09fe35a000bf20a2681c62e4ceed
NovaProtocol <khyles.gibrian.ramos19@gmail.com>
2021-08-27 01:58:11 +0800

    Update

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from git import Repo
+
 repo = Repo()
+
 branch = 'main'
 repo.remotes.origin.fetch()
-commits_diff = repo.git.rev_list('--left-right', '--count', f'{branch}...{branch}@{{u}}')
-print(commits_diff)
-
-print(sum(1 for _ in repo.iter_commits('main..origin/main')))
+x = sum(1 for _ in repo.iter_commits('main..origin/main'))
+repo.remotes.origin.pull()
