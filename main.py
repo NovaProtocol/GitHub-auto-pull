797e4256f6ec242af01992925f8dcb787885bafd
NovaProtocol <35219142+NovaProtocol@users.noreply.github.com>
2021-08-27 20:38:30 +0800

    Added a clone feature
    It now allows working even without doing any setup

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
-from git import Repo
+from git import Repo, Git, exc
 import time
+import shutil
+import os
 
-repo = Repo()
-
+git_username = 'NovaProtocol'
+git_repository = 'GitHub-auto-pull'
 branch = 'main'
-repo.remotes.origin.fetch()
-x = sum(1 for _ in repo.iter_commits('main..origin/main'))
-repo.remotes.origin.pull()
-print('done')
-time.sleep(5)
 
-#edit current file
+try:
+    repo = Repo()
+except exc.InvalidGitRepositoryError:
+    Git().clone(f"git://github.com/{git_username}/{git_repository}.git")
+    file_names = os.listdir(f'{os.getcwd()}/{git_repository}')
+    for file_name in file_names:
+        if file_name == __file__.split('\\')[-1]:
+            pass
+        else:
+            shutil.move(os.path.join(f'{os.getcwd()}/{git_repository}', file_name), os.getcwd())
+    repo = Repo()
+print('Intialization Complete')
+while True:
+    repo.remotes.origin.fetch()
+    if sum(1 for _ in repo.iter_commits('main..origin/main')):
+        repo.remotes.origin.pull()
+        print('Pulled from repo')
+    time.sleep(1)
